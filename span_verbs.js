2c28a91a3f39d5603a06b52f4de1d93d7a627e4f
Andy-Stacks <andyatuni@outlook.com>
2020-08-22 23:23:17 +0100

    span_verbs.css - minor css changes span_verbs.html, span_verbs.js: - various comments have been added - hints have been removed - a skip question button has been added that displays correct answer - verbs objects has been moved to its own .js file - verbs object has been restructured so that multiple translations are possible for words - overall js structure has been made more modular and understandable

--- span_verbs.js
+++ span_verbs.js
@@ -1,89 +1,112 @@
-let verbs = {
-    0:['Ser','To be'], 1:['Haber','To have'], 2:['Estar','To be'], 3:['Tener','To have'], 4:['Hacer','To do'], 5:['Poder','To be able'], 6:['Decir','To say'], 7:['Ir','To go'], 8:['Ver','To see'], 9:['Dar','To give'], 10:['Saber','To know'], 11:['Querer','To want'], 12:['Llegar','To arrive'], 13:['Pasar','To pass'], 14:['Deber','To owe'], 15:['Poner','To put'], 16:['Parecer','To seem'], 17:['Quedar','To stay'], 18:['Creer','To believe'], 19:['Hablar','To speak'], 20:['Llevar','To carry'], 21:['Dejar','To leave'], 22:['Seguir','To follow'], 23:['Encontrar','To find'], 24:['Llamar','To call']
-}
-let e_hints = {
-    0:'Quiero ser alto', 1:'1', 2:'¿Quieres estar feliz?', 3:'Es bueno tener paciencia', 4:'Tengo que hacer todo', 5:'Quiero poder hablar Español', 6:'Qué quieres decir', 7:'¿Quieres ir ahora?', 8:'Voy a ver a mi amiga', 9:'Necesito darle mis llaves', 10:'¿Quieres saber la respuesta?', 11:'Siempre pareces querer una pista', 12:'Nosotros vamos a llegar mañana', 13:'Tenemos que pasar por aquí', 14:'No quiero deber dinero', 15:'Lo voy a poner sobre la mesa', 16:'Quiere parecer inteligente', 17:'Tengo que quedarme aqui', 18:'Quiero creer que entenderás esto', 19:'El profesor necesita hablar con el estudiante', 20:'¿Quieres llevar un diccionario para siempre?', 21:'¿Qué quieres dejar atrás?', 22:'No es bueno seguir a la gente', 23:'¿Estás tratando de encontrar una pista?', 24:'Necesito llamar a mi jefa'
-}
-let s_hints = {
-    0:'I want to be tall', 1:'1', 2:'Do you want to be happy?', 3:'It is good to have patience', 4:'I have to do everything', 5:'I want to be able to speak Spanish', 6:'What do you want to say?', 7:'Do you want to go now?', 8:'I am going to see my friend', 9:'I need to give you my keys', 10:'Do you want to know the answer?', 11:'You always seem to want a hint', 12:'We are going to arrive tomorrow', 13:'We have to pass through here', 14:'I do not want to owe money', 15:'I am going to put it on the table', 16:'He wants to seem intelligent', 17:'I have to stay here', 18:'I want to believe that you will understand this', 19:'The teacher needs to speak with the student', 20:'Do you want to carry a dictionary forever?', 21:'What do you want to leave behind?', 22:'It is not good to follow people', 23:'Are you trying to find a hint?', 24:'I need to call my boss'
-}
+//global variables
+var currentScore = 0;
+var highScore = localStorage.getItem('highScore') || 0;
+var wordIdx = 0;
+var engToSpan = false; //true = user must translate from english to spanish
+var language = 'english';
 
 $(document).ready( () => { 
-    $(function () {
-        $('[data-toggle="popover"]').popover()
-    })
-    let language = 0;
-    let wordIdx = getNextWordIdx();
-    let engToSpan = false;
-    let currentScore = 0;
-    let highScore = localStorage.getItem('highScore');
-
+    //setup game
     $('#current-score').text(`Current Streak: ${currentScore}`);
-    $('#high-score').text(`Highest Streak: ${highScore || 0}`);
+    $('#high-score').text(`Highest Streak: ${highScore}`);
+    nextQuestion();
 
-    nextQuestion(wordIdx,engToSpan,language);
-    $('#answer').focus();
-    $('#answer').keypress((e) => {if (e.which == 13){$('#submit-answer').click()}});
+    //call change language and set current language to return value
+    $('#language').click(() => changeLanguage());
 
-    $('#language').click(() => language = changeLanguage(language,engToSpan,currentScore,highScore));
-
-    $('#submit-answer').click( () => {
-        if ($('#answer').val().trim().toLowerCase() == verbs[wordIdx][engToSpan ? 0 : 1].toLowerCase()) { 
-            currentScore++;
-            engToSpan = !engToSpan;
-            wordIdx = getNextWordIdx();
-            $('#question-card').popover('hide');
-            $('#question-card').addClass('text-white bg-success');
-            setTimeout(() => nextQuestion(wordIdx,engToSpan,language), 1500);
-            if (currentScore > highScore){highScore = currentScore; localStorage.setItem('highScore', highScore)}
-        }
-        else {
-            currentScore = 0;
-            $('#wrong-answer').fadeIn();
-            setTimeout(() => $('#wrong-answer').fadeOut(), 2000);
-        }
-        updateScores(currentScore,highScore,language);
-    })
+    //answer submission
+    $('#submit-answer').click( () => submitAnswer());
+    $('#answer').keypress((e) => {if (e.which == 13){submitAnswer()}});
+    
+    //skip question
+    $('#skip-question').click( () => skipQuestion());
 })
 
-function nextQuestion(wordIdx,engToSpan,language){
-    $('#answer').val('');
-    $('#answer').focus();
-    $('#question-card').removeClass('text-white bg-success mb-3');
-    $('#word-title').text(verbs[wordIdx][engToSpan ? 1 : 0]);
-    $('#question-card').attr('data-content',engToSpan ? s_hints[wordIdx] : e_hints[wordIdx]);
-    language ? $('#answer-title').text(`Traducir a ${engToSpan ? 'Español:' : 'Ingles:'}`)
-    : $('#answer-title').text(`Translate to ${engToSpan ? 'Spanish:' : 'English:'}`);
-}
+// <--- utility functions --->
+
+// <--- choose translation at random --->
 function getNextWordIdx(){
     return Math.floor(Math.random() * 25); 
 }
-function changeLanguage(language,engToSpan,currentScore,highScore){
-    if (language) {
+
+// <--- retrieve next question --->
+function nextQuestion(){
+    wordIdx = getNextWordIdx();
+    $('#answer').val('');
+    $('#answer').focus();
+    $('#question-card').removeClass('text-white bg-success bg-warning');
+
+    //access verb object, access either eng or span array, set text to random word from array (capitalised)
+    const wordsArray = verbs[wordIdx][engToSpan ? 1 : 0];
+    const words = wordsArray[Math.floor(Math.random() * wordsArray.length)];
+    $('#word-title').text(capitalise(words));
+
+    //set instructions based on language setting
+    language == 'spanish' ? $('#answer-title').text(`Traducir a ${engToSpan ? 'Español:' : 'Ingles:'}`)
+    : $('#answer-title').text(`Translate to ${engToSpan ? 'Spanish:' : 'English:'}`);
+}
+function capitalise(words){
+    return words.replace(/\w+\S/g, (word) => word.charAt(0).toUpperCase() + word.substr(1).toLowerCase());
+}
+
+// <--- submit answer --->
+function submitAnswer(){
+    //check if answer is correct
+    if (verbs[wordIdx][engToSpan ? 0 : 1].includes($('#answer').val().trim().toLowerCase())) { 
+        currentScore++;
+        engToSpan = !engToSpan;
+        $('#question-card').addClass('text-white bg-success');
+        setTimeout(() => nextQuestion(), 1500);
+        //update highscore if necessary
+        if (currentScore > highScore){
+            highScore = currentScore; 
+            localStorage.setItem('highScore', highScore)
+        }
+    }
+    else {
+        currentScore = 0;
+        $('#wrong-answer').fadeIn();
+        setTimeout(() => $('#wrong-answer').fadeOut(), 2000);
+    }
+    updateScores();
+}
+
+// <--- skip question --->
+function skipQuestion(){
+    currentScore = 0;
+    $('#question-card').addClass('bg-warning');
+    $('#word-title').text(capitalise(verbs[wordIdx][engToSpan ? 0 : 1][0]));
+    setTimeout(() => nextQuestion(), 3000);
+}
+
+// <--- toggle application language --->
+function changeLanguage(){
+    if (language == 'spanish') {
+        language = 'english';
         $('#language').text('Español');
         $('#submit-answer').text('Submit');
-        $('#tip').text('Click here for an example!');
         $('#answer').attr('placeholder','Translate...');
         $('#answer-title').text(`Translate to ${engToSpan ? 'Spanish:' : 'English:'}`);
     }
     else {
+        language = 'spanish';
         $('#language').text('English');
         $('#submit-answer').text('Enviar');
-        $('#tip').text('Haga clic aquí para ver un ejemplo!');
         $('#answer').attr('placeholder','Traducir...');
         $('#answer-title').text(`Traducir a ${engToSpan ? 'Español:' : 'Ingles:'}`);
     }
-    updateScores(currentScore,highScore,!language);
+    updateScores();
     $('#answer').focus();
-    return !language;
 }
-function updateScores(currentScore,highScore,language){
-    if (language) {
+
+// <--- update score (language setting checked) --->
+function updateScores(){
+    if (language == 'spanish') {
         $('#current-score').text(`Victorias Consecutivas: ${currentScore}`);
-        $('#high-score').text(`Mejor Racha: ${highScore || 0}`);
+        $('#high-score').text(`Mejor Racha: ${highScore}`);
     }
     else {
         $('#current-score').text(`Current Streak: ${currentScore}`);
-        $('#high-score').text(`Highest Streak: ${highScore || 0}`);
+        $('#high-score').text(`Highest Streak: ${highScore}`);
     }
 }
